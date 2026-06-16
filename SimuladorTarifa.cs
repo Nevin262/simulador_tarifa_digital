9413b6c77932e179d605bcdf3591fef334a25502
Nevin262 <nevin.daviterrones@gmail.com>
2026-06-16 00:41:58 -0500

    feat: implementar calculo base y logica de hora pico, descuento y resumen de viaje

--- SimuladorTarifa.cs
+++ SimuladorTarifa.cs
@@ -7,16 +7,20 @@ public class SimuladorTarifa
         Console.WriteLine("================================");
 
         //Ingreso de datos
+        //Declaración de variables
+        string nombre, nombreVehiculo;
+        double distancia, tarifaBase, costoKm, subtotal, descuento, tarifaFinal;
+        int hora, tipoVehiculo;
+        bool esHoraPico;
+
         Console.WriteLine("Nombre del Pasajero:");
-        string nombre = Console.ReadLine();
+        nombre = Console.ReadLine();
 
         Console.WriteLine("Ingrese distancia en km:");
-        double distancia = double.Parse(Console.ReadLine());
+        distancia = double.Parse(Console.ReadLine());
 
         Console.WriteLine("Hora de salida (0 hrs - 23 hrs):");
-        int hora = int.Parse(Console.ReadLine());
-
-        string tipoVehiculo = Console.ReadLine();
+        hora = int.Parse(Console.ReadLine());
 
         Console.WriteLine("\nTipo de Vehículo: ");
         Console.WriteLine("1. Económico");
@@ -24,9 +28,74 @@ public class SimuladorTarifa
         Console.WriteLine("3. Premium");
         Console.WriteLine("4. Moto");
         Console.WriteLine("Seleccione el tipo de vehículo (1-4):");
-        int tipoVehiculo = int.Parse(Console.ReadLine());
+        tipoVehiculo = int.Parse(Console.ReadLine());
 
         //Proceso
 
+        switch (tipoVehiculo)
+        {
+            case 1: // Económico
+                nombreVehiculo = "Económico";
+                tarifaBase = 2.0;
+                costoKm = 1.5;
+                break;
+            case 2:// Confort
+                nombreVehiculo = "Confort";
+                tarifaBase = 3.0;
+                costoKm = 2.0;
+                break;
+            case 3:// Premium
+                nombreVehiculo = "Premium";
+                tarifaBase = 5.0;
+                costoKm = 3.0;
+                break;
+            case 4:// Moto
+                nombreVehiculo = "Moto";
+                tarifaBase = 1.5;
+                costoKm = 1.0;
+                break;
+            default:
+                Console.WriteLine("\nOpción no válida. Finalizando simulación.");
+                return;
+        }
+
+        // Cálculo de Subtotal Base
+        subtotal = tarifaBase + (costoKm * distancia);
+        // Condicional de Recargo Hora Pico
+        if((hora >=7 && hora <=9) || (hora >=17 && hora <=20))
+        {
+            esHoraPico = true;
+            subtotal = subtotal * 1.30; // Recargo del 30%
+        }
+
+        //Descuento por distancia larga (5%)
+        if(distancia > 15)
+        {
+            descuento = subtotal * 0.05;
+            subtotal = subtotal - descuento;
+        }
+
+        // Tarifa mínima y redondeo
+        tarifaFinal = Math.Max(subtotal, 5.00);
+        tarifaFinal = Math.Round(tarifaFinal, 2);
+
+        //Salida - Resumen de viaje
+        Console.WriteLine("\n================================");
+        Console.WriteLine("Resumen de la Simulación:");
+        Console.WriteLine("================================");
+        Console.WriteLine("Pasajero: " + nombre);
+        Console.WriteLine("Vehículo: " + nombreVehiculo);
+        Console.WriteLine("Distancia: " + distancia + " km");
+        Console.WriteLine("Hora Pico: " + (esHoraPico ? "Sí (+30%)" : "No"));
+        if(descuento > 0)
+        {
+            Console.WriteLine("Descuento: S/ " + Math.Round(descuento, 2));
+        }else
+        {
+            Console.WriteLine("Descuento: No aplica");
+        }
+
+        Console.WriteLine("================================");
+        Console.WriteLine("Tarifa Final: S/ " + tarifaFinal);
     }
 }
